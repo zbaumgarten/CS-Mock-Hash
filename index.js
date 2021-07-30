ab6ded4c651ae0718f5cb8ce23da665209b1b1f8
brandiw <brandimichelle.0911@gmail.com>
2021-07-30 15:31:09 -0700

    Starter code

---
+++ index.js
@@ -0,0 +1,5 @@
+// This allows us to use the bcrypt algorithm in our Node.js project
+const bcrypt = require('bcrypt')
+
+// This allows us to read from the terminal
+const readline = require('readline')
